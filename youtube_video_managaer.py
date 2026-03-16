d44aead40fb0b72dc220aa9815981bef1fd75a04
Engr-Jawad <jawadbuniri2@gmail.com>
2026-03-16 22:28:25 +0500

    project half completed

--- youtube_video_managaer.py
+++ youtube_video_managaer.py
@@ -1,29 +1,59 @@
+import json
 def load_data():
-    pass
+    try:
+        with open ('youtube.txt','r') as file:
+           test =  json.load(file)
+        return test
+           
+    except FileNotFoundError :
+        return []
+
+
+def save_data(videos):
+    with open ('youtube.txt','w') as file :
+        json.dump(videos,file)
+
+
 
 def list_all_videos(videos):
-    pass
+    for index,video in enumerate(videos,start=1):
+        print("\n")
+        print(f"{index}.  Video Name : {video['name']}, Durations : {video['time']}")
+        print("_" * 30)
 
 def add_videos(videos):
-    pass
+    name=input("enter the video name : ")
+    time=input("enter the video length : ")
+    videos.append({'name':name,'time':time})
+    save_data(videos)
+
+
+
 
 def  update_videos(videos):
     pass
 
+
+
+
 def delete_videos(videos):
     pass
 
+
+
+
 def main():
-    videos=[]
+    videos=load_data()
     while(True):
-        print("welcome to the youtube manager app ")
+        print("YOUTUBE MANAGER APP")
         print("1.list of the videos ")
         print("2.add youtube videos")
         print("3.update a youtube videos ")
         print("4.delete videos")
         print("5.exit")
-        print("choose your option in the above")
-        choice=int(input("enter  your choice here : "))
+        # print("choose your option in the above")
+        choice=(input("enter  your choice here : "))
+        
         
         match choice:
             case '1':
